eeae43ff2eaae4e55f19625912eb45767fbcdd6e
Rifky Arifin <int-rifky@lalampa.me>
2024-10-07 16:12:33 +0700

    Penambahkan Komponen Information

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
 import Navbar from "@/components/Navbar";
-import ProdukCloud from "@/components/ProdukCloud"
+import ProdukCloud from "@/components/ProdukCloud";
+import Information from "@/components/Information";
 
 const Page = () => {
     return (
         <>
             <Navbar />
             <ProdukCloud/>
-            
+            <Information/> 
         </>
        
     );
