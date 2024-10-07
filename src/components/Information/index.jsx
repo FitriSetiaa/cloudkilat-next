461c388ede4c76d570e4adfdbd24ba8b7d6614ef
Rifky Arifin <int-rifky@lalampa.me>
2024-10-07 16:13:17 +0700

    Membuat Komponen Produk dan Information

---
+++ src/components/Information/index.jsx
@@ -0,0 +1,111 @@
+import Image from "next/image";
+
+const Information = () => {
+  return (
+    <div className="relative p-5 sm:p-10 nunito">
+      <div className="flex justify-between items-center mb-10">
+        <strong><h1 className="text-[#136AAD] text-3xl">INFORMASI UMUM</h1></strong>
+        <a href="">
+        <div className="flex items-center gap-2">
+          <p className="hidden sm:block">Tampilkan Semua</p>
+          <Image
+            className="cursor-pointer"
+            src="/images/arrow.png"
+            alt="Arrow"
+            width={20}
+            height={16}
+          />
+        </div>
+        </a>
+      </div>
+      <div className="font-bold">
+        <a href="">
+        <div className="flex gap-3 text-[16px] sm:text-[20px] hover:text-[#136AAD] mb-2">
+            <Image 
+            className=""
+            src="/images/logo_cloudkilat.png"
+            alt="Cloudkilat"
+            width={25}
+            height={20}
+            />
+            <h2 className="flex-1 truncate">Mengenal FileZilla dan Cara Instalasinya dengan Mudah</h2>
+        </div>
+        </a>
+        <a href="">
+        <div className="flex gap-3 text-[16px] sm:text-[20px] hover:text-[#136AAD] mb-2">
+            <Image 
+            className=""
+            src="/images/logo_cloudkilat.png"
+            alt="Cloudkilat"
+            width={25}
+            height={20}
+            />
+            <h2 className="flex-1 truncate">Upgrade In-Place CentOS 7 ke AlmaLinux 8 dengan ELevate</h2>
+        </div>
+        </a>
+        <a href="">
+        <div className="flex gap-3 text-[16px] sm:text-[20px] hover:text-[#136AAD] mb-2">
+            <Image 
+            className=""
+            src="/images/logo_cloudkilat.png"
+            alt="Cloudkilat"
+            width={25}
+            height={20}
+            />
+            <h2 className="flex-1 truncate">Panduan Lengkap: Cara Instal LEMP Stack (Nginx, MySQL, PHP) di KILAT VM 2.0 dengan OneinStack</h2>
+        </div>
+        </a>
+        <a href="">
+        <div className="flex gap-3 text-[16px] sm:text-[20px] hover:text-[#136AAD] mb-2">
+            <Image 
+            className=""
+            src="/images/logo_cloudkilat.png"
+            alt="Cloudkilat"
+            width={25}
+            height={20}
+            />
+            <h2 className="flex-1 truncate">Cara Menginstall Plesk di AlmaLinux 8 dengan One-Click Installer</h2>
+        </div>
+        </a>
+        <a href="">
+        <div className="flex gap-3 text-[16px] sm:text-[20px] hover:text-[#136AAD] mb-2">
+            <Image 
+            className=""
+            src="/images/logo_cloudkilat.png"
+            alt="Cloudkilat"
+            width={25}
+            height={20}
+            />
+            <h2 className="flex-1 truncate">Cara Menginstal Control Panel HestiaCP pada Ubuntu'22.04</h2>
+        </div>
+        </a>
+        <a href="">
+        <div className="flex gap-3 text-[16px] sm:text-[20px] hover:text-[#136AAD] mb-2">
+            <Image 
+            className=""
+            src="/images/logo_cloudkilat.png"
+            alt="Cloudkilat"
+            width={25}
+            height={20}
+            />
+            <h2 className="flex-1 truncate">Mengatasi Kendala apt-get update “the following signatures couldn't be verified because the public key is not available” di Ubuntu</h2>
+        </div>
+        </a>
+        <a href="">
+        <div className="flex gap-3 text-[16px] sm:text-[20px] hover:text-[#136AAD] mb-2">
+            <Image 
+            className=""
+            src="/images/logo_cloudkilat.png"
+            alt="Cloudkilat"
+            width={25}
+            height={20}
+            />
+            <h2 className="flex-1 truncate">Upgrade Ubuntu 20.04 ke Ubuntu 22.04 pada VPS Plesk</h2>
+        </div>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Information;
--- src/components/ProdukCloud/index.jsx
+++ src/components/ProdukCloud/index.jsx
@@ -1,30 +1,167 @@
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card"
-
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Image from "next/image";
 
 const Page = () => {
-    return (
-        <>
-            <Card className='bg-gray-400'>
-                <CardHeader>
-                    <CardTitle>Card Title</CardTitle>
-                    <CardDescription>Card Description</CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <p>Card Content</p>
-                </CardContent>
-                <CardFooter>
-                    <p>Card Footer</p>
-                </CardFooter>
-            </Card>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="p-10 bg-[#F2FAFF] nunito">
+        <strong>
+          <h1 className="text-3xl mb-10 text-[#136AAD]">PORTAL CLOUDKILAT</h1>
+        </strong>
+          <Image
+            className="absolute right-0 lg:mt-0 sm:mt-[250px]"
+            src="/images/elips1.png"
+            alt="people"
+            width={440}
+            height={440}
+          />
+          <Image
+            className="absolute right-0 lg:mt-[70px] sm:mt-[320px]"
+            src="/images/elips2.png"
+            alt="people"
+            width={320}
+            height={320}
+          />
+          <Image
+            className="absolute right-0 lg:mt-[140px] sm:mt-[390px]"
+            src="/images/elips2.png"
+            alt="people"
+            width={210}
+            height={210}
+          />
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 justify-items-center relative overflow-hidden">
+          <Card className="w-[281px] h-[221px] flex flex-col justify-center bg-transparent border border-blue-500 hover:bg-[#136AAD] hover:text-white transition-colors duration-300">
+            <a href="">
+            <CardContent className="flex items-center space-x-5">
+              <Image
+                className=""
+                src="/images/people.png"
+                alt="people"
+                width={50}
+                height={50}
+              />
+              {/* White Version */}
+              {/* <Image 
+                className="hidden hover:block"
+                src="/images/people-white.png"
+                alt="people white"
+                width={50}
+                height={50}
+            /> */}
+              <strong>
+                <h2 className="text-lg">Akun</h2>
+              </strong>
+            </CardContent>
+            <CardFooter className="">
+              <p>
+                Mengelola informasi pribadi seperti kredensial, profil dan
+                preferensi.
+              </p>
+            </CardFooter>
+            </a>
+          </Card>
+
+          <Card className="w-[281px] h-[221px] flex flex-col justify-center bg-transparent hover:bg-[#136AAD] hover:text-white transition-colors duration-300">
+            <a href="">
+            <CardContent className="flex items-center space-x-5">
+              <Image
+                className=""
+                src="/images/bag.png"
+                alt="people"
+                width={50}
+                height={50}
+              />
+              {/* White Version */}
+              {/* <Image 
+                className="hidden hover:block"
+                src="/images/people-white.png"
+                alt="people white"
+                width={50}
+                height={50}
+            /> */}
+              <strong>
+                <h2 className="text-lg">Pemesanan</h2>
+              </strong>
+            </CardContent>
+            <CardFooter className="">
+              <p>
+                Memilih layanan, menentukan spesifikasi, dan mengaktifkan paket
+                dengan cepat.
+              </p>
+            </CardFooter>
+            </a>
+          </Card>
+
+          <Card className="w-[281px] h-[221px] flex flex-col justify-center bg-transparent border border-blue-500 hover:bg-[#136AAD] hover:text-white transition-colors duration-300">
+            <a href="">
+            <CardContent className="flex items-center space-x-5">
+              <Image
+                className=""
+                src="/images/wallet.png"
+                alt="people"
+                width={50}
+                height={50}
+              />
+              {/* White Version */}
+              {/* <Image 
+                className="hidden hover:block"
+                src="/images/people-white.png"
+                alt="people white"
+                width={50}
+                height={50}
+            /> */}
+              <strong>
+                <h2 className="text-lg">Pembayaran</h2>
+              </strong>
+            </CardContent>
+            <CardFooter className="">
+              <p>
+              Memilih opsi pembayaran yang sesuai, CloudKilat mendukung berbagai metode transaksi yang aman dan cepat.
+              </p>
+            </CardFooter>
+            </a>
+          </Card>
+
+          <Card className="w-[281px] h-[221px] flex flex-col justify-center bg-transparent border border-blue-500 hover:bg-[#136AAD] hover:text-white transition-colors duration-300">
+            <a href="">
+            <CardContent className="flex items-center space-x-5">
+              <Image
+                className=""
+                src="/images/check_list.png"
+                alt="people"
+                width={50}
+                height={50}
+              />
+              {/* White Version */}
+              {/* <Image 
+                className="hidden hover:block"
+                src="/images/people-white.png"
+                alt="people white"
+                width={50}
+                height={50}
+            /> */}
+              <strong>
+                <h2 className="text-lg">Tiket</h2>
+              </strong>
+            </CardContent>
+            <CardFooter className="">
+              <p>
+              Mengelola permintaan bantuan terkait kendala teknis atau informasi lainnya.
+              </p>
+            </CardFooter>
+            </a>
+          </Card>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Page;
